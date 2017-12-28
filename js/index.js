ce258f2ffdc07792f7eb69ac68a41a9cf10c669b
Tonyhew <916390127@qq.com>
2017-12-28 19:23:12 +0800

    js

--- js/index.js
+++ js/index.js
@@ -37,77 +37,7 @@ $(document).ready(function(){
 		$(".navlist").find("."+names).hide();
 		});
 		
-	/*新品首发*/
-	/*$(".newspan span").eq(0).addClass("current");	
-	$(".newimg a").eq(0).show().siblings("a").hide();
-	$(".newspan span").mouseover(function(){
-		numb=$(this).index();
-		$(this).addClass("current").siblings("span").removeClass("current");
-		$(".newimg a").eq(numb).fadeIn().siblings("a").fadeOut();
-		});
-	setInterval(function(){
-		var a=$(".newspan .current").index();
-		var b=$(".newspan span:last").index();
-		if(a==b){
-			$(".newspan span").eq(0).trigger("mouseover");
-			}
-			$(".newspan span").eq(a+1).trigger("mouseover");
-		},5000);*/
 	
-	/*兑换专区*/
-	/*$(".change").mouseover(function(){
-		$(".changezuo").show();
-		$(".changeyou").show();
-		}).mouseout(function(){
-			$(".changezuo").hide();
-			$(".changeyou").hide();
-			});
-	var widthd=$(".changecon").width();
-	var paged=1;
-	var id=5;
-	var numd=$(".changelist dl").length;
-	var pagemaxd=Math.ceil(numd/id);
-	$(".changeyou").click(function(){
-		if(!$(".changelist").is(":animated")){
-		if(paged==pagemaxd){
-			$(".changelist").animate({"left":"0"},500);
-			paged=1;
-		}else{
-			$(".changelist").animate({"left":"-="+widthd+"px"},500);
-			paged++;
-			}
-		}
-	});
-	$(".changezuo").click(function(){
-		if(!$(".changelist").is(":animated")){
-		if(paged==1){
-			$(".changelist").animate({"left":-widthd*(pagemaxd-1)+"px"},500);
-			paged=pagemaxd;
-		}else{
-			$(".changelist").animate({"left":"+="+widthd+"px"},500);
-			paged--;
-			}
-		}
-	});
-	setInterval(function(){
-		$(".changeyou").trigger("click");
-	},5000);*/
-	
-	/*销量最多*/
-	/*$(".mostspan span").eq(0).addClass("current");
-	$(".mostproduct").eq(0).show().siblings(".mostproduct").hide();
-	$(".mostspan span").click(function(){
-		$(this).addClass("current").siblings("span").removeClass("current");
-		numcc=$(this).index();
-		$(".mostproduct").eq(numcc).show().siblings(".mostproduct").hide();
-		});
-	$(".mostproduct").each(function(){
-		$(this).find("li dl").mouseover(function(){
-			$(this).find(".empty").hide().parents("li").siblings("li").find("dl .empty").show();
-			}).mouseout(function(){
-				$(this).parents("li").siblings("li").find("dl .empty").hide();
-				});
-		});	*/
 	
 	/*福利模块 大图切换*/
 	$(".welfarezuo").each(function(){
@@ -126,7 +56,7 @@ $(document).ready(function(){
 		});
 	
 	/*热销排行*/
-	/*$(".welfareyou").each(function(){
+	$(".welfareyou").each(function(){
 		$(this).find(".hotcon:not(:first)").each(function(){
 			$(this).find("ul").hide();
 			});
@@ -137,5 +67,5 @@ $(document).ready(function(){
 				$(this).find("dl").hide().parents(".hotcon").siblings(".hotcon").find("dl").show();
 				});
 			});
-		});*/
+		});
 });
